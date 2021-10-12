2e85a76a5bb366aad591d4c588d16c1ff7e715d5
tucno21 <carlitostucno@gmail.com>
2021-10-12 06:26:51 -0500

    migracion countries

---
+++ app/Database/Migrations/2021-10-12-110909_Countries.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Countries extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'          => [ //nombre de la columna
+                'type'           => 'INT', //tipo de columna
+                'constraint'     => 11, // tamaño de la columna
+                'unsigned'       => true,
+                'auto_increment' => true,
+                'null' => false, //no nulo
+            ],
+            'name'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => '80',
+                'null' => false, //no nulo
+            ],
+            'create_at' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true); //asigna id primario
+        $this->forge->createTable('countries'); //nombre de la tabla a crear
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('countries'); //nombre de la tabla //este seccion elimina la tabla
+    }
+}
