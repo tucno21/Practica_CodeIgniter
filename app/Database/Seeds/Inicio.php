6c3a1363c20d64cd03322724d85866e94a2d11ac
tucno21 <carlitostucno@gmail.com>
2021-10-12 08:29:08 -0500

    instanciando faker

--- app/Database/Seeds/Inicio.php
+++ app/Database/Seeds/Inicio.php
@@ -3,11 +3,12 @@
 namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
+use Faker\Factory;
 
 class Inicio extends Seeder
 {
     public function run()
     {
-        //
+        $faker = Factory::create();
     }
 }
