f6bfcb3942fbbf21d0e9f36443a569cccfbd7370
tucno21 <carlitostucno@gmail.com>
2021-10-13 20:58:10 -0500

    base para los seeder

--- app/Database/Seeds/CountriesSeeder.php
+++ app/Database/Seeds/CountriesSeeder.php
@@ -2,12 +2,31 @@
 
 namespace App\Database\Seeds;
 
+use Faker\Factory;
 use CodeIgniter\Database\Seeder;
 
 class CountriesSeeder extends Seeder
 {
     public function run()
     {
-        //
+        $faker = Factory::create();
+        $db      = \Config\Database::connect();
+
+        $countries = [];
+
+        for ($i = 0; $i < 15; $i++) {
+            $create_at = $faker->dateTime();
+            $updated_at = $faker->dateTimeBetween($create_at);
+
+            $countries[] = [
+                'name' => $faker->country,
+                'create_at' => $create_at->format('Y-m-d H:i:s'),
+                'updated_at' => $updated_at->format('Y-m-d H:i:s'),
+            ];
+        }
+
+        //https://codeigniter4.github.io/userguide/database/query_builder.html#inserting-data
+        $builder = $db->table('countries');
+        $builder->insertBatch($counters);
     }
 }
--- app/Database/Seeds/GroupsSeeder.php
+++ app/Database/Seeds/GroupsSeeder.php
@@ -2,12 +2,33 @@
 
 namespace App\Database\Seeds;
 
+use Faker\Factory;
 use CodeIgniter\Database\Seeder;
 
 class GroupsSeeder extends Seeder
 {
     public function run()
     {
-        //
+        $faker = Factory::create();
+        $db      = \Config\Database::connect();
+
+
+        $fecha = $faker->dateTime();
+        $group = [
+            [
+                'name' => 'admin',
+                'create_at' => $fecha->format('Y-m-d H:i:s'),
+                'updated_at' => $faker->dateTimeBetween($fecha)->format('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'user',
+                'create_at' => $fecha->format('Y-m-d H:i:s'),
+                'updated_at' => $faker->dateTimeBetween($fecha)->format('Y-m-d H:i:s'),
+            ],
+        ];
+
+        //https://codeigniter4.github.io/userguide/database/query_builder.html#inserting-data
+        $builder = $db->table('groups');
+        $builder->insertBatch($group);
     }
 }
