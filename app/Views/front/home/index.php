4e170a88ba69c49261376a0256cbf4f80a345975
tucno21 <carlitostucno@gmail.com>
2021-10-16 07:50:03 -0500

    eliminar la etiquetas html

--- app/Views/front/home/index.php
+++ app/Views/front/home/index.php
@@ -31,8 +31,8 @@ Home
                             <h5 class="card-title"><?= $post->title ?></h5>
                             <h8 class="card-title"><?= $post->nameInfo . " " . $post->surnameInfo ?></h8>
                             <p class="card-text">
-                                Some quick example text to build on the card title and make up the bulk of the
-                                card's content.
+                                <!-- https://www.php.net/manual/es/function.strip-tags.php -->
+                                <?= strip_tags($post->body) ?>
                             </p>
                             <a href="#!" class="btn btn-primary">Read</a>
                         </div>
